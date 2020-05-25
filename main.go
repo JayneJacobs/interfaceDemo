14a70d8302ad42ab29362f83fb67781299b77ec3
jjacob151 <jayne_jacobs@cable.comcast.com>
2020-05-25 11:51:14 -0400

    Updating Readme.md go.mod interfaces main.go

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "fmt"
 type person struct{
 	first string
 }
+
+type human interface {
+	speak()
+}
+
 func (p person) speak() {
 	fmt.Printf("Hi I am %s and here is my type %T \n", p.first, p)
 }
@@ -15,4 +20,10 @@ func main() {
  }
 
  person1.speak()
+
+ var h human
+
+ h = person1
+
+ h.speak()
 }
